6c2eafafc47d5c8d6cd5c09c8ca3b8fbad13d2d9
SahidGuardado <cguardadoz@unah.hn>
2025-08-01 01:52:50 -0600

    popups varios(faltan detalles)
    Popups para la implementacion de nuevas asignaturas y creacion de nuevas tareas

--- lib/src/config/router/app_router.dart
+++ lib/src/config/router/app_router.dart
@@ -1,9 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:my_homework_app/src/pages/agg_asignatura_page.dart';
 import 'package:my_homework_app/src/pages/bienvenida_page.dart';
 import 'package:my_homework_app/src/pages/calendar_page.dart';
 import 'package:my_homework_app/src/pages/home_page.dart';
 import 'package:my_homework_app/src/pages/login_page.dart';
+import 'package:my_homework_app/src/pages/new_tarea_page.dart';
 import 'package:my_homework_app/src/pages/notificacion_page.dart';
+import 'package:my_homework_app/src/pages/popups.dart';
 import 'package:my_homework_app/src/pages/registro_page.dart';
 
 final appRouter = GoRouter(
@@ -11,6 +15,21 @@ final appRouter = GoRouter(
     GoRoute(path: '/', builder: (context, state) => BienvenidaPage()),
     GoRoute(path: '/login', builder: (context, state) => LoginPage()),
     GoRoute(path: '/registro', builder: (context, state) => RegistroPage()),
+    GoRoute(path: '/popup', builder: (context, state) => Popuppage(context)as Widget),
+    GoRoute(
+                name: 'agregar-asignatura',
+                path: '/login/agregar-asignatura',
+                builder: (context, state) {
+                  return CrearAsignatura();
+                },
+              ),
+               GoRoute(
+                name: 'nueva-tarea',
+                path: '/login/nueva-tarea',
+                builder: (context, state) {
+                  return CrearTarea(asignaturas: [],);
+                },
+              ),
 
     GoRoute(path: '/home', builder: (context, state) => HomePage()),
     GoRoute(
